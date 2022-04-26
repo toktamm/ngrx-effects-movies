7877e43eb1bc3302e2a94a22f1a1881c20c8346a
Toktam <ttmohebbi@gmail.com>
2022-04-26 17:33:17 -0400

    remove some of the ts errors

--- src/app/movies-page.component.ts
+++ src/app/movies-page.component.ts
@@ -1,3 +1,6 @@
+import { Component } from '@angular/core';
+import { MoviesService } from './movies.service';
+
 @Component({
   template: `
     <li *ngFor="let movie of movies">
@@ -6,11 +9,13 @@
   `,
 })
 export class MoviesPageComponent {
-  movies: Movie[];
+  movies: Movie[] = [];
 
   constructor(private movieService: MoviesService) {}
 
   ngOnInit() {
-    this.movieService.getAll().subscribe((movies) => (this.movies = movies));
+    this.movieService
+      .getAll()
+      .subscribe((movies: Movie[]) => (this.movies = movies));
   }
 }
